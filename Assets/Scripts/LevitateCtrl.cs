e624604ea744f01fc839c38ebc21fc857e302e49
Alex Dinu <adix64@gmail.com>
2020-11-16 17:40:13 +0200

    primu nostru commit

---
+++ Assets/Scripts/LevitateCtrl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevitateCtrl : MonoBehaviour
+{
+    float initialY; //pozitia initiala pe axa verticala
+    public float freq = 1f; // cat de repede oscileaza levitatorul sus-jos
+    public float amplitude = 0.25f; //cat de larg oscileaza
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posY = initialY + Mathf.Sin(Time.time * freq) * amplitude; // oscilatia pe verticala
+
+        transform.position = new Vector3(transform.position.x,
+                                        posY, // doar coordonata verticala se schimba
+                                        transform.position.z);
+    }
+}
---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float moveSpeed = 2f; // viteza de deplasare a personajului
+  
+    // Apelat o singura data, la inceputul jocului sau cand obiectul e activat/spawnat
+    void Start()
+    {
+    }
+    // Update e apelat de N ori pe secunda, N fluctuant, preferabil N > 60FPS
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal"); // -1 pentru tasta A, 1 pentru tasta D, 0 altfel
+        float v = Input.GetAxis("Vertical"); // -1 pentru tasta S, 1 pentru tasta W, 0 altfel
+
+        Vector3 dir = new Vector3(h, 0, v);
+        dir = dir.normalized; // lungime 1 pentru orice Vector3 ce reprezinta directie
+
+        //deplasamentul trebuie sa fie proportional cu timpul scurs intre 2 cadre...
+        //...ca sa putem mentine viteza independent de framerate
+        Vector3 offset = dir * Time.deltaTime * moveSpeed; //deplasamentul intre cadre...
+        transform.position += offset; //se aduna la pozitia personajului
+    }
+}
